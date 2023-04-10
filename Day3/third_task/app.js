b90705fa13b8b2ae6324d46ca2125f43abd4311d
RithickRavisubramanian <rithickr459@gmail.com>
2023-04-10 21:35:51 +0530

    added my new countdown timer task in day 3

---
+++ Day3/third_task/app.js
@@ -0,0 +1,20 @@
+
+const countDownDateTime =  new Date(2024,1,1,0,0,0).getTime();
+const daysValue = document.querySelector("#days");
+const hoursValue = document.querySelector("#hours");
+const minutesValue = document.querySelector("#minutes");
+const secondsValue = document.querySelector("#seconds");
+
+let x = setInterval(function(){
+    const dateTimeNow = new Date().getTime();
+    let difference = countDownDateTime - dateTimeNow;
+
+    daysValue.innerHTML = Math.floor(difference /(1000 * 60 * 60 * 24));
+    hoursValue.innerHTML = Math.floor(difference % (1000 * 60 * 60 * 24)/(1000 * 60 * 60));
+    minutesValue.innerHTML = Math.floor(difference %(1000 * 60 * 60)/(1000 * 60));
+    secondsValue.innerHTML = Math.floor(difference %(1000 * 60 )/1000);
+
+    if(difference < 0){
+        clearInterval(x);
+    }
+},1000);
